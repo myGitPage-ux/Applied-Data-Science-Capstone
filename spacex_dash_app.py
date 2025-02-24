b147daa8b053bcc0f359a3e51adac5c9adf39d4e
myGitPage-ux <sandeepk123@gmail.com>
2025-02-24 19:39:36 +0530

    Add files via upload

---
+++ spacex_dash_app.py
@@ -0,0 +1,60 @@
+# Import required libraries
+import pandas as pd
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Create an app layout
+app.layout = html.Div(children=[
+    html.H1('SpaceX Launch Records Dashboard',
+            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+    
+    # TASK 1: Add a dropdown list to enable Launch Site selection
+    dcc.Dropdown(
+        id='site-dropdown',
+        options=[
+            {'label': 'All Sites', 'value': 'ALL'},
+            {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+            {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
+            {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+            {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'}
+        ],
+        value='ALL',  # Default value
+        placeholder="Select a Launch Site",
+        searchable=True
+    ),
+
+    html.Br(),
+
+    # TASK 2: Add a pie chart
+    html.Div(dcc.Graph(id='success-pie-chart')),
+    html.Br(),
+
+    html.P("Payload range (Kg):"),
+    
+    # TASK 3: Add a slider to select payload range
+    dcc.RangeSlider(
+        id='payload-slider',
+        min=min_payload, max=max_payload, step=1000,
+        marks={i: str(i) for i in range(int(min_payload), int(max_payload) + 1, 2000)},
+        value=[min_payload, max_payload]
+    ),
+
+    html.Br(),
+
+    # TASK 4: Add a scatter chart
+    html.Div(dcc.Graph(id='success-payload-scatter-chart'))
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
